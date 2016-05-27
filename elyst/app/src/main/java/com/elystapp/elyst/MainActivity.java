af2f5f91fa3365ca839067de68cfacfbd5f99be5
roxana <roxana.c.gheorghe@gmail.com>
2016-05-27 15:16:11 -0400

    fab button

--- elyst/app/src/main/java/com/elystapp/elyst/MainActivity.java
+++ elyst/app/src/main/java/com/elystapp/elyst/MainActivity.java
@@ -85,8 +85,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent create_event = new Intent(MainActivity.this, CreateEventActivity.class);
+                startActivity(create_event);
             }
         });
 
